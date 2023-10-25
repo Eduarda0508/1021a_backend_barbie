ea5d3a140b1d15426ac9a07ddeff3a291f566321
Eduarda0508 <maria.silva87@estudante.ifms.edu.br>
2023-10-25 08:03:29 -0400

    aula 25/10

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,9 +13,26 @@ describe('Cadastro Filme', () => {
             descricao: 'Filme dos Vingadores',
             foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
         }
-        const resposta = 
-            await axios.post('http://localhost:3000/filmes', filme)
+        const resposta = await axios.post('http://localhost:3000/filmes', filme)
         expect(resposta.status).toBe(201)
         expect(resposta.data).toEqual(filme)
     })
+    it('Deve listar um filme cadastrado', async () => {
+      const filme = {
+          titulo: 'Vingadores',
+          descricao: 'Filme dos Vingadores',
+          foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
+      }
+      const resposta =  await axios.post('http://localhost:3000/filmes', filme)
+
+      //1) Buscar o filme cadastrado
+      const listaFilmes= await axios.get('http://localhost:3000/filmes')
+      const filmeCadastrado = listaFilmes.data
+
+      //2) Verificar se o filme devolvido é igual ao que nós cadastramos
+      expect(filmeCadastrado[0]).toEqual(filme)
+
+      //3)Verificar se o tamanho da lista é igual a 1
+      expect(filmeCadastrado.lenght).toBe(1)
+  })
 })
