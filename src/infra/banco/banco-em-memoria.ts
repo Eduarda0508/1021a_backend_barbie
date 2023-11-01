10ba1f9d65c5cb2dd7fbbdcdc077a6dd0e0bb644
Eduarda0508 <maria.silva87@estudante.ifms.edu.br>
2023-11-01 08:16:34 -0400

    Adicionando a interface na classe BancoemMemoria

--- src/infra/banco/banco-em-memoria.ts
+++ src/infra/banco/banco-em-memoria.ts
@@ -1,9 +1,18 @@
-export default class BancoEmMemoria{
-    public dados:any[] = []
+import FilmeRepositorioInterface from "../../aplicacao/filme-repositorio-interface"
+export default class BancoEmMemoria implements FilmeRepositorioInterface{
+    public dados:Filme[] = []
     constructor(){}
-    public salvar(input){  
-     
+    public salvar(input:Filme):Promise<boolean>{  
     this.dados.push(input)
-    return true
+    return new Promise ((resolve,reject)=>{
+        resolve(true)
+        })
     }
+}
+
+type Filme = {
+    id: number,
+    titulo: string,
+    descricao: string,
+    foto: string
 }
