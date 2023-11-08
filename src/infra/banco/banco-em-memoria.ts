8b215e6e83fa12217dd9574dcf37105ac8591fc7
Eduarda0508 <maria.silva87@estudante.ifms.edu.br>
2023-11-08 08:27:51 -0400

    aula 08/11

--- src/infra/banco/banco-em-memoria.ts
+++ src/infra/banco/banco-em-memoria.ts
@@ -1,18 +1,17 @@
-import FilmeRepositorioInterface from "../../aplicacao/filme-repositorio-interface"
+import FilmeRepositorioInterface from "../../aplicacao/filme-repositorio-interface";
 export default class BancoEmMemoria implements FilmeRepositorioInterface{
     public dados:Filme[] = []
     constructor(){}
-    public salvar(input:Filme):Promise<boolean>{  
-    this.dados.push(input)
-    return new Promise ((resolve,reject)=>{
-        setTimeout(()=>resolve(true),500)
-    })
+    public salvar(input:Filme):Promise<boolean>{
+        this.dados.push(input)
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>resolve(true),10)
+        })
     }
 }
-
 type Filme = {
-    id: number,
-    titulo: string,
-    descricao: string,
-    foto: string
+    id:number,
+    titulo:string,
+    descricao:string,
+    foto:string
 }
